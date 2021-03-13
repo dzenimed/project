4ac7cd35ee011339501ba837553f7bef9e275a0c
dzenimed <dzenana.mededovic@gmail.com>
2021-03-13 17:49:57 +0100

    config change

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
-"# autoresponder"
+"# recipeBook"
 
 hello!
--- api/config.php
+++ api/config.php
@@ -2,8 +2,8 @@
 
 class Config {
   const DB_HOST = "localhost"; // or $ENV[] instead of ""; loads data from environment file;
-  const DB_USERNAME = "root";
-  const DB_PASSWORD = "root123";
+  const DB_USERNAME = "recipeBook";
+  const DB_PASSWORD = "recipeBook123";
   const DB_SCHEME = "mydb";
 }
 
