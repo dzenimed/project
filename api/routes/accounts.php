dd8f12f22816dd78ee3e3c5fb06d4116c73e5f35
dzenimed <dzenana.mededovic@gmail.com>
2021-04-08 23:05:04 +0200

    body fixes in account route

--- api/routes/accounts.php
+++ api/routes/accounts.php
@@ -8,8 +8,13 @@
  * )
  */
 
+
 /**
- * @OA\Get(path="/accounts",
+ * @OA\Get(path="/accounts", tags={"account"},
+ *     @OA\Parameter(@OA\Schema(type="integer"), in="query", name="offset", default=0, description="Offset for pagination"),
+ *     @OA\Parameter(@OA\Schema(type="integer"), in="query", name="limit", default=25, description="Limit for pagination"),
+ *     @OA\Parameter(@OA\Schema(type="string"), in="query", name="search", description="Search string for accounts. Case insensitive search."),
+ *     @OA\Parameter(@OA\Schema(type="string"), in="query", name="order", default="-id", description="Sorting for return elements. -column_name ascending order by column_name or +column_name descending order by column_name"),
  *     @OA\Response(response="200", description="List accounts from database")
  * )
  */
