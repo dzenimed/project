d7cfdd8f05b13c2bda078602d7195bd822d61eda
dzenimed <dzenana.mededovic@gmail.com>
2021-04-08 20:00:32 +0200

    add get and update of recipes

--- api/routes/recipes.php
+++ api/routes/recipes.php
@@ -6,9 +6,12 @@ Flight::route('GET /recipes', function(){
   $recipe_name = Flight::query('recipe_name');
   $search = Flight::query('search');
   $order = Flight::query('order', '-id');
-  Flight::json(Flight::recipeService()->get_recipe($recipe_name, $offset, $limit, $search, $order));
 
-//  Flight::json(Flight::recipeService()->get_recipe());
+  Flight::json(Flight::recipeService()->get_recipe($recipe_name, $offset, $limit, $search, $order));
+});
+
+Flight::route('GET /recipes/@id', function($id){
+    Flight::json(Flight::recipeService()->get_by_id($id));
 });
 
 // Doesn't work properly, Check why v5, 30 min
@@ -17,6 +20,9 @@ Flight::route('POST /recipes', function(){
   Flight::json(Flight::recipeService()->add($data));
 });
 
-
+Flight::route('PUT /recipes/@id', function($id){
+  $data = Flight::request()->data->getData();
+  Flight::json(Flight::recipeService()->update($id, $data));
+});
 
 ?>
