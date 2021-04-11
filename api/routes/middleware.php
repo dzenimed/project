cd2457387991ff671c40a72e10fd4a2adae9f345
dzenimed <dzenana.mededovic@gmail.com>
2021-04-11 22:53:51 +0200

    add middleware protection to api

---
+++ api/routes/middleware.php
@@ -0,0 +1,24 @@
+<?php
+
+Flight::before('start', function(&$params, &$output){
+
+  if(Flight::request()->url == '/swagger') return TRUE;
+
+  if(str_starts_with(Flight::request()->url == '/users/')) return TRUE;
+
+  $headers = getallheaders();
+  $token = @$headers['Authentication'];
+  try {
+    $decoded = (array)\Firebase\JWT\JWT::decode($token,"JWT SECRET", ['HS256']);
+    Flight::set('user', $decoded);
+    // ADMIN -> set of routes for admin
+    // USER  -> set of routes for user
+    // USER_READ_ONLY -> set of routes except for PUT or POST
+    return TRUE;
+  } catch (\Exception $e) {
+    Flight::json(["message" => $e->getMessage()], 401);
+    die;
+  }
+});
+
+?>
