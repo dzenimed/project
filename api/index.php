5d3399f4309b46280b718395ac5a01bee78b6ee9
dzenimed <dzenana.mededovic@gmail.com>
2021-06-23 10:27:10 +0200

    server side data table without pagination

--- api/index.php
+++ api/index.php
@@ -24,7 +24,7 @@ Flight::map('query', function($name, $default_value = NULL){
   $request = Flight::request();
   $query_param = @$request->query->getData()[$name];
   $query_param = $query_param ? $query_param : $default_value;
-  return $query_param;
+  return urldecode($query_param);
 });
 
 
