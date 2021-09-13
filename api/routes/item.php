37c0f2b858cecfca596c35e73b047c939663a9d2
dzenimed <dzenana.mededovic@gmail.com>
2021-09-13 16:16:20 +0200

    typo fix

--- api/routes/item.php
+++ api/routes/item.php
@@ -4,7 +4,7 @@
  * @OA\Get(path="/user/item", tags={"x-user", "item"}, security={{"ApiKeyAuth":{}}},
  *     @OA\Parameter(@OA\Schema(type="integer"), in="query", name="offset", default=0, description="Offset for pagination"),
  *     @OA\Parameter(@OA\Schema(type="integer"), in="query", name="limit", default=25, description="Limit for pagination"),
- *     @OA\Parameter(@OA\Schema(type="string"), in="query", recipe_name="search", description="Search string for item. Case insensitive search."),
+ *     @OA\Parameter(@OA\Schema(type="string"), in="query", name="search", description="Search string for item. Case insensitive search."),
  *     @OA\Parameter(@OA\Schema(type="string"), in="query", name="order", default="-id", description="Sorting for return elements. -column_name ascending order by column_name or +column_name descending order by column_name"),
  *     @OA\Response(response="200", description="List items from database")
  * )
