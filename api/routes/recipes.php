9cce763799e56cacd863bb304d52336d7e7ef1a8
dzenimed <dzenana.mededovic@gmail.com>
2021-09-20 13:21:19 +0200

    code refactoring, add update route

--- api/routes/recipes.php
+++ api/routes/recipes.php
@@ -1,7 +1,7 @@
 <?php
 
 /**
- * @OA\Get(path="/user/recipes", tags={"x-user", "recipes"}, security={{"ApiKeyAuth":{}}},
+ * @OA\Get(path="recipes", tags={"recipes"}, security={{"ApiKeyAuth":{}}},
  *     @OA\Parameter(@OA\Schema(type="integer"), in="query", name="offset", default=0, description="Offset for pagination"),
  *     @OA\Parameter(@OA\Schema(type="integer"), in="query", name="limit", default=25, description="Limit for pagination"),
  *     @OA\Parameter(@OA\Schema(type="string"), in="query", name="search", description="Search string for recipes. Case insensitive search."),
@@ -9,7 +9,7 @@
  *     @OA\Response(response="200", description="List recipes from database")
  * )
  */
-Flight::route('GET /user/recipes', function(){
+Flight::route('GET /recipes', function(){
   $offset = Flight::query('offset', 0);
   $limit = Flight::query('limit', 25);
   $search = Flight::query('search');
@@ -26,7 +26,7 @@ Flight::route('GET /user/recipes', function(){
  *       @OA\MediaType(mediaType="application/json",
  *    			@OA\Schema(
 *    		       @OA\Property(property="recipe_name", requierd=true, type="string", example="RecipeName", description="Name of the recipe"),
-*    		       @OA\Property(property="preparation_steps", requierd=true, type="int", example="Preheat the oven to... Prepare the..", description="Step by step guide to making the recipe"),
+*    		       @OA\Property(property="preparation_steps", requierd=true, type="string", example="Preheat the oven to... Prepare the..", description="Step by step guide to making the recipe"),
 *              @OA\Property(property="tips", requierd=true, type="string", example="My tip is to ...", description="Tips and tricks to make the recipe preparation easier")
  *             )
  *       )
@@ -37,6 +37,25 @@ Flight::route('GET /user/recipes', function(){
 // Add not working
 Flight::route('POST /user/recipes', function(){
   Flight::json(Flight::recipeService()->add_recipe(Flight::request()->data->getData()));
+});
+
+/**
+ * @OA\Put(path="/recipes/{id}", tags={"recipes"}, security={{"ApiKeyAuth": {}}},
+ *   @OA\Parameter(type="integer", in="path", name="id", default=1),
+ *   @OA\RequestBody(description="Basic recipe info that is going to be updated", required=true,
+ *       @OA\MediaType(mediaType="application/json",
+ *    			@OA\Schema(
+ *    			@OA\Property(property="recipe_name", requierd=true, type="string", example="RecipeName", description="Name of the recipe"),
+ *    		  @OA\Property(property="preparation_steps", requierd=true, type="string", example="Preheat the oven to... Prepare the..", description="Step by step guide to making the recipe"),
+ *          @OA\Property(property="tips", requierd=true, type="string", example="My tip is to ...", description="Tips and tricks to make the recipe preparation easier")
+ *          )
+ *       )
+ *     ),
+ *     @OA\Response(response="200", description="Update recipe")
+ * )
+ */
+Flight::route('PUT /recipes/update/@id', function($id){
+  Flight::json(Flight::recipeService()->update(intval($id), Flight::request()->data->getData()));
 });
 
 ?>
